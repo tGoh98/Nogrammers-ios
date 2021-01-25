20fc0352f56fe146c04c955cc325ac3fefee78c3
tGoh98 <tmg5@rice.edu>
2021-01-25 16:26:16 -0600

    Initial Commit

---
+++ Nogrammers-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nogrammers-ios
+//
+//  Created by Timothy Goh on 1/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
